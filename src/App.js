6da142c8d4c03de080517a91fa8955301e89ab3c
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-10-25 22:22:00 -0500

    Agregando firebase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Chat from "./pages/Chat";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { auth } from "./services/firebase";
+import './style/style.css';
 
 function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
